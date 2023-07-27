15746deabfa6342ccd0b00001274e271ae689fd2
LaudRam <laudiciaramasenya@gmail.com>
2023-07-27 12:43:03 +0200

    Updated for week 7
    Switched to SheCodes weather API, Added unit conversion to Fahrenheit, API weather icon, description and weather conditions also added

--- style/app.js
+++ style/app.js
@@ -26,32 +26,61 @@ currentTime();
 
 function currentTemp(response) {
   let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
+  h1.innerHTML = response.data.city;
+  let celciusTemperature = response.data.temperature.current;
+  //let temperature = Math.round(celciusTemperature);
   let tempValue = document.querySelector("#temp-value");
-  tempValue.innerHTML = temperature;
+  //tempValue.innerHTML = temperature;
+  tempValue.innerHTML = Math.round(celciusTemperature);
 }
+
+function tempCelcius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temp-value");
+
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  temperature.innerHTML = Math.round(celciusTemperature);
+}
+let celciusTemperature = null;
+let celciusLink = document.querySelector("#celcius");
+celciusLink.addEventListener("click", tempCelcius);
+
+function tempFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temp-value");
+
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheitTemperature);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", tempFahrenheit);
 
 function currentConditions(response) {
   let description = document.querySelector("#description");
-  description.innerHTML = response.data.weather[0].main;
+  description.innerHTML = response.data.condition.description;
 
   let windSpeed = Math.round(response.data.wind.speed * 3.6);
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = `Wind: ${windSpeed}km/h`;
 
-  let humidity = response.data.main.humidity;
+  let humidity = response.data.temperature.humidity;
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = `Humidity: ${humidity}%`;
+
+  let icon = response.data.condition.icon_url;
+  document.querySelector("img").src = icon;
 }
 
 function search(city) {
-  let apiKey = "689cf53aaaf224213ca60529e012e6c1";
-  let URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+  let apiKey = "0f09bat43dccc526f81281do161a0bf2";
+  let URL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(URL).then(currentTemp);
   axios.get(URL).then(currentConditions);
 }
-search(document.querySelector("h1").innerHTML);
+search("Bushbuckridge");
 
 function searchBar(event) {
   event.preventDefault();
@@ -64,8 +93,8 @@ form.addEventListener("submit", searchBar);
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let apiKey = "689cf53aaaf224213ca60529e012e6c1";
-  let URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&&units=metric`;
+  let apiKey = "0f09bat43dccc526f81281do161a0bf2";
+  let URL = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
   axios.get(URL).then(currentTemp);
   axios.get(URL).then(currentConditions);
